b85c8e366ab40f79f17f4a787518037ebd26f3b8
Rob Duarte <me@robduarte.com>
2025-01-27 13:46:45 -0500

    Switched from pflag to kong

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/rahji/timeblocker
 go 1.23.3
 
 require github.com/spf13/pflag v1.0.5
+
+require github.com/alecthomas/kong v1.6.1 // indirect
--- main.go
+++ main.go
@@ -6,26 +6,31 @@ import (
 	"strings"
 	"time"
 
-	flag "github.com/spf13/pflag"
+	"github.com/alecthomas/kong"
 )
 
+type Flags struct {
+	Start    string `kong:"required,help='Start time of work day (HH:MM)'"`
+	End      string `kong:"required,help='End time of work day (HH:MM)'"`
+	Duration int    `kong:"required,help='Duration of each meeting slot, in minutes'"`
+	Break    int    `kong:"default=5,help='Duration of the break between each meeting, in minutes'"`
+	Csv      bool   `kong:"default=false,help='Print schedule in CSV format instead of Markdown'"`
+}
+
 func main() {
-	startTime := flag.String("start", "09:00", "Start time of work day (HH:MM)")
-	endTime := flag.String("end", "17:00", "End time of work day (HH:MM)")
-	duration := flag.Int("duration", 30, "Duration of each meeting slot, in minutes")
-	buffer := flag.Int("buffer", 5, "Duration of the break between each meeting, in minutes")
-	csv := flag.Bool("csv", false, "Print schedule in CSV format")
-	flag.Parse()
+	var flags Flags
+	ctx := kong.Parse(&flags)
+	_ = ctx
 
 	// Parse start time
-	start, err := time.Parse("15:04", *startTime)
+	start, err := time.Parse("15:04", flags.Start)
 	if err != nil {
 		fmt.Printf("Invalid start time format: %v\n", err)
 		os.Exit(1)
 	}
 
 	// Parse end time
-	end, err := time.Parse("15:04", *endTime)
+	end, err := time.Parse("15:04", flags.End)
 	if err != nil {
 		fmt.Printf("Invalid end time format: %v\n", err)
 		os.Exit(1)
@@ -37,7 +42,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if *duration <= 0 {
+	if flags.Break <= 0 {
 		fmt.Println("Duration must be positive")
 		os.Exit(1)
 	}
@@ -46,7 +51,7 @@ func main() {
 
 	current := start
 	for !current.After(end) {
-		slotEnd := current.Add(time.Duration(*duration) * time.Minute)
+		slotEnd := current.Add(time.Duration(flags.Duration) * time.Minute)
 
 		// Don't include slots that extend beyond the end time
 		if slotEnd.After(end) {
@@ -54,7 +59,7 @@ func main() {
 		}
 
 		format := "| %-10s | %-10s |\n"
-		if *csv {
+		if flags.Csv {
 			format = "%s,%s\n"
 		}
 
@@ -63,10 +68,10 @@ func main() {
 			slotEnd.Format("15:04")),
 		)
 
-		current = slotEnd.Add(time.Duration(*buffer) * time.Minute)
+		current = slotEnd.Add(time.Duration(flags.Break) * time.Minute)
 	}
 
-	if !*csv {
+	if !flags.Csv {
 		fmt.Print("\n# Meeting Schedule\n\n")
 		fmt.Print("| Start Time | End Time   |\n")
 		fmt.Print("|------------|------------|\n")
